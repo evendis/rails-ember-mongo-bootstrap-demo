7cf616d96bee0d0740b98473529ad206892f0fca
Paul Gallagher <gallagher.paul@gmail.com>
2012-03-18 19:53:36 +0800

    init - base rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsEmberMongoBootstrapDemo::Application.config.secret_token = '4cc7711694788b5aedcec490e364905d3988d65b9edf01bc9282f0f20de9b41cade2c4529097466468d10b1a17d8353e2aca68ce2a953ed1b77998b1d5c934e9'
